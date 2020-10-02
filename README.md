040935250b57b0dddecf7bf2cc9a9324f04ce92c
Rahul Ahire <ahirerahul21@gmail.com>
2020-10-02 19:04:58 +0530

    project finished

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Multiple Image Preview
+
+Credits: Code Taken and modified from [Zipzat](https://github.com/zipzit/React-Multiple-Image-Upload-and-Preview)
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+const App = () => {
+	const [ selectedFiles, setSelectedFiles ] = useState([]);
+
+	const handleImageChange = (e) => {
+		// console.log(e.target.files[])
+		if (e.target.files) {
+			const filesArray = Array.from(e.target.files).map((file) => URL.createObjectURL(file));
+
+			// console.log("filesArray: ", filesArray);
+
+			setSelectedFiles((prevImages) => prevImages.concat(filesArray));
+			Array.from(e.target.files).map(
+				(file) => URL.revokeObjectURL(file) // avoid memory leak
+			);
+		}
+	};
+
+	const renderPhotos = (source) => {
+		console.log('source: ', source);
+		return source.map((photo) => {
+			return <img src={photo} alt="" key={photo} />;
+		});
+	};
+
+	return (
+		<div className="app">
+			<div className="heading">React Multiple Images Preview</div>
+			<div>
+				<input type="file" id="file" multiple onChange={handleImageChange} />
+				<div className="label-holder">
+					<label htmlFor="file" className="label">
+						<i class="material-icons">add_a_photo</i>
+					</label>
+				</div>
+				<div className="result">{renderPhotos(selectedFiles)}</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
